748d902badb2e1d8129d7efce90cb9b43a5d1f04
Chris Phillips <chris.phillips@mindmatrix.net>
2018-07-02 12:35:24 -0400

    updating build with source link

--- build/build.bat
+++ build/build.bat
@@ -1,1 +1,1 @@
-dotnet build -c Release /p:VERSION=%1
+dotnet build -c Release /p:VERSION=%1 /p:SourceLinkCreate=true
--- build/pack.bat
+++ build/pack.bat
@@ -1,3 +1,1 @@
-FOR /D /r %%G in ("src\obj\Release\*") DO gitlink "src\obj\Release\%%~nxG\MindMatrix.Kamen.pdb"
-FOR /D /r %%G in ("src\bin\*") DO gitlink "src\bin\%%~nxG\MindMatrix.Kamen.pdb"
-dotnet pack src\Kamen\MindMatrix.Kamen.csproj --include-symbols --no-build -c Release /p:VERSION=%1
+dotnet pack src\Kamen\MindMatrix.Kamen.csproj --no-build -c Release /p:VERSION=%1
